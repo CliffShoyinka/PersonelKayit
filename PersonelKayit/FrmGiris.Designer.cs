fb8874fc4353a89c50399f1d6e519f21258083fe
Mehmethan Bellek <bellekmehmethan@gmail.com>
2023-10-03 00:13:38 +0300

    3th november

--- PersonelKayit/FrmGiris.Designer.cs
+++ PersonelKayit/FrmGiris.Designer.cs
@@ -30,10 +30,10 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TxtKullaniciAd = new System.Windows.Forms.TextBox();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BtnGirisYap = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -54,19 +54,19 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "sifre";
             // 
-            // textBox1
+            // TxtKullaniciAd
             // 
-            this.textBox1.Location = new System.Drawing.Point(203, 123);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(149, 20);
-            this.textBox1.TabIndex = 2;
+            this.TxtKullaniciAd.Location = new System.Drawing.Point(203, 123);
+            this.TxtKullaniciAd.Name = "TxtKullaniciAd";
+            this.TxtKullaniciAd.Size = new System.Drawing.Size(149, 20);
+            this.TxtKullaniciAd.TabIndex = 2;
             // 
-            // textBox2
+            // TxtSifre
             // 
-            this.textBox2.Location = new System.Drawing.Point(203, 192);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(149, 20);
-            this.textBox2.TabIndex = 3;
+            this.TxtSifre.Location = new System.Drawing.Point(203, 192);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(149, 20);
+            this.TxtSifre.TabIndex = 3;
             // 
             // label3
             // 
@@ -77,24 +77,25 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Personel takip sistemi giris";
             // 
-            // button1
+            // BtnGirisYap
             // 
-            this.button1.Location = new System.Drawing.Point(181, 256);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Giris Yap";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtnGirisYap.Location = new System.Drawing.Point(181, 256);
+            this.BtnGirisYap.Name = "BtnGirisYap";
+            this.BtnGirisYap.Size = new System.Drawing.Size(115, 23);
+            this.BtnGirisYap.TabIndex = 5;
+            this.BtnGirisYap.Text = "Giris Yap";
+            this.BtnGirisYap.UseVisualStyleBackColor = true;
+            this.BtnGirisYap.Click += new System.EventHandler(this.BtnGirisYap_Click);
             // 
             // FrmGiris
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(489, 341);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnGirisYap);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.TxtKullaniciAd);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "FrmGiris";
@@ -108,9 +109,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TxtKullaniciAd;
+        private System.Windows.Forms.TextBox TxtSifre;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnGirisYap;
     }
 }
